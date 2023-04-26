f2acd42bf5e53941be56c5cf0405e5b86188f5dc
24HourDeveloper <latoya30310@gmail.com>
2023-04-26 11:08:20 -0400

    added products and props

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,14 +3,8 @@ import './SearchBar.css'
 import { clear } from '@testing-library/user-event/dist/clear'
 
 
-const products = [
-  'tooth paste',
-  'tooth brush',
-  'mouth wash',
-  'dental floss',
-  'mouth guard',
-]
-const SearchBar = () => {
+
+const SearchBar = (props) => {
 
   const [searchValue, setSearchValue] = useState('')
 
@@ -24,13 +18,17 @@ const SearchBar = () => {
 
   const shouldDisplayButton = searchValue.length > 0
 
+  const filteredProducts = props.products.filter((product) => {
+    return product.includes(searchValue)
+  })
+
   return (
     <div>
       <input type='text' value={searchValue} onChange={handleInputChange} />
       {shouldDisplayButton && <button onClick={handleClearClick}>clear</button>}
       
       <ul>
-        {products.map((product) => {
+        {filteredProducts.map((product) => {
           return <li key={product}>{product}</li>
         })}
       </ul>
--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,30 @@
 import React from "react"
-// import CountButton from "./CountButton/CountButton"
+import CountButton from "./CountButton/CountButton"
 import SearchBar from "./SearchBar/SearchBar"
+
 
 const App = () => {
 
   return ( 
     <div>
-      <SearchBar/>
-      {/* <CountButton incrementBy={1} buttonColor={'red'} borderRadius={'10px'} /> */}
-      {/* <CountButton incrementBy={5} buttonColor={'lightBlue'} borderRadius={'20px'} /> */}
+      <SearchBar 
+      const products = {[
+        'tooth paste',
+        'tooth brush',
+        'mouth wash',
+        'dental floss',
+        'mouth guard',
+      ]}/>
+      <SearchBar 
+      const products = {[
+        'bike rake',
+        'bike brush',
+        'tire pump',
+        'soccer ball',
+        'bike guard',
+      ]}/>
+      <CountButton incrementBy={1} buttonColor={'red'} borderRadius={'10px'} />
+      <CountButton incrementBy={5} buttonColor={'lightBlue'} borderRadius={'20px'} />
     </div>
   )
 }
